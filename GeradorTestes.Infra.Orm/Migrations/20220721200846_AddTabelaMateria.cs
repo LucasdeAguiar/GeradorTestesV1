21b13953ccf86972f42f886dcf38217dfbb03756
Lucas de Aguiar <lucasaguiaresteves@outlook.com>
2022-07-22 13:27:51 -0300

    Add project files.

---
+++ GeradorTestes.Infra.Orm/Migrations/20220721200846_AddTabelaMateria.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GeradorTestes.Infra.Orm.Migrations
+{
+    public partial class AddTabelaMateria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "TBDisciplina",
+                type: "varchar(100)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "TBMateria",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Serie = table.Column<int>(type: "int", nullable: false),
+                    DisciplinaId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBMateria", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TBMateria_TBDisciplina_DisciplinaId",
+                        column: x => x.DisciplinaId,
+                        principalTable: "TBDisciplina",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBMateria_DisciplinaId",
+                table: "TBMateria",
+                column: "DisciplinaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBMateria");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "TBDisciplina",
+                type: "varchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)");
+        }
+    }
+}
---
+++ GeradorTestes.Infra.Orm/Migrations/20220721205204_AddTabelaQuestao.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GeradorTestes.Infra.Orm.Migrations
+{
+    public partial class AddTabelaQuestao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TBMateria_TBDisciplina_DisciplinaId",
+                table: "TBMateria");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DisciplinaId",
+                table: "TBMateria",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "TBQuestao",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Enunciado = table.Column<string>(type: "varchar(500)", nullable: false),
+                    MateriaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBQuestao", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TBQuestao_TBMateria_MateriaId",
+                        column: x => x.MateriaId,
+                        principalTable: "TBMateria",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TBAlternativa",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Correta = table.Column<bool>(type: "bit", nullable: false),
+                    Letra = table.Column<string>(type: "nvarchar(1)", nullable: false),
+                    QuestaoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Resposta = table.Column<string>(type: "varchar(500)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBAlternativa", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TBAlternativa_TBQuestao",
+                        column: x => x.QuestaoId,
+                        principalTable: "TBQuestao",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBAlternativa_QuestaoId",
+                table: "TBAlternativa",
+                column: "QuestaoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBQuestao_MateriaId",
+                table: "TBQuestao",
+                column: "MateriaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TBMateria_TBDisciplina_DisciplinaId",
+                table: "TBMateria",
+                column: "DisciplinaId",
+                principalTable: "TBDisciplina",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TBMateria_TBDisciplina_DisciplinaId",
+                table: "TBMateria");
+
+            migrationBuilder.DropTable(
+                name: "TBAlternativa");
+
+            migrationBuilder.DropTable(
+                name: "TBQuestao");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DisciplinaId",
+                table: "TBMateria",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TBMateria_TBDisciplina_DisciplinaId",
+                table: "TBMateria",
+                column: "DisciplinaId",
+                principalTable: "TBDisciplina",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
